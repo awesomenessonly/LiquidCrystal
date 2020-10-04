3b8b4223894dbd6de9b29085c9f222d761175d64
eucalvo <eliamcalvo24@gmail.com>
2020-10-04 14:18:26 -0700

    start on autoscroll test

--- test/TestClass.cpp
+++ test/TestClass.cpp
@@ -14,4 +14,32 @@ unittest(getRows) {
   assertEqual(2, lcd.getRows());
 }
 
+// based on Autoscroll.ino example
+unittest(autoscrollHi){
+  LiquidCrystal_Test lcd(rs, enable, d4, d5, d6, d7);
+
+  // set the cursor to (0,0):
+  lcd.setCursor(0, 0);
+  // print from 0 to 9:
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+    lcd.print(thisChar);
+    delay(500);
+  }
+
+  // set the cursor to (16,1):
+  lcd.setCursor(16, 1);
+  // set the display to automatically scroll:
+  lcd.autoscroll();
+  // print from 0 to 9:
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+    lcd.print(thisChar);
+    delay(500);
+  }
+  // turn off automatic scrolling
+  lcd.noAutoscroll();
+
+  // clear screen for the next loop:
+  lcd.clear();
+}
+
 unittest_main()
